9b16c6905841df64db0c89054c208c0d8ac3e6cb
Admin <Admin@DESKTOP-6PQE64G>
2024-01-15 11:24:32 +0530

    Add project files.

---
+++ count array/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class arrays
+{
+    static void Main(string[] args)
+    {
+        int[] n = { 1, 2, 3, 4, 5, 6, 7, 8 };
+        int count = 0;
+        foreach(int x in n)
+        {
+            count++;
+        }
+        Console.WriteLine(count);
+    }
+    
+}
